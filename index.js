4ca4f470f69a63da80e2e689f206abfec2b9a33b
DESKTOP-U6817H5\user <melzone554@hotmail.com>
2019-01-01 22:31:40 +0800

    UPDATE port 8888 to 8080

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http')
 var url = require('url')
 
-http.createServer(onRequest).listen(8888);
+http.createServer(onRequest).listen(8080);
 console.log('Server has started');
 
 function onRequest(request, response){
